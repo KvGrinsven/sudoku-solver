3ee105f78aabb034af4e422c2f98bdaac761a463
Kyra van Grinsven <kvgrinsven@sogyo.nl>
2020-07-17 16:00:00 +0200

    Add method to find options for a given position

--- src/main/java/nl/sogyo/javaopdrachten/sudoku/Solver.java
+++ src/main/java/nl/sogyo/javaopdrachten/sudoku/Solver.java
@@ -1,5 +1,8 @@
 package nl.sogyo.javaopdrachten.sudoku;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solver {
 	
 	int[] board;
@@ -17,6 +20,58 @@ public class Solver {
 	
 	// TODO Implement, dah :P
 	public boolean solve() {return true;}
+	
+	
+	private List<Integer> getOptions(int pos) {
+		
+		List<Integer> options = new ArrayList<>();
+		options.add(1);
+		options.add(2);
+		options.add(3);
+		options.add(4);
+		options.add(5);
+		options.add(6);
+		options.add(7);
+		options.add(8);
+		options.add(9);
+		
+		int x = pos % 9;
+		int y = pos / 9;
+		int boxTopLeft = pos - (x % 3) - (y % 3 * 9);
+		
+		removeForRow(options, pos - x);
+		removeForColumn(options, y);
+		removeForBox(options, boxTopLeft);
+		
+		return options;
+	}
+	
+	private void removeForRow(List<Integer> list, int start) {
+		for(int i = start; i < start + 9; i++) {
+			Integer value = board[i];
+			list.remove(value);
+		}
+	}
+	
+	private void removeForColumn(List<Integer> list, int y) {
+		for(int i = y; i < 81; i += 9) {
+			Integer value = board[i];
+			list.remove(value);
+		}
+	}
+	
+	private void removeForBox(List<Integer> list, int topleft) {
+		for(int i = topleft; i < topleft+3; i++) {
+			Integer v1 = board[i];
+			list.remove(v1);
+			
+			Integer v2 = board[i+9];
+			list.remove(v2);
+			
+			Integer v3 = board[i+18];
+			list.remove(v3);
+		}
+	}
 	
 	
 	// TODO Add actual | and - stuff, if I want to
